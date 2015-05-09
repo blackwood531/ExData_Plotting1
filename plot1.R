1392a09218076ac14d506f3d97f77fd47ed87733
blackwood531 <don@columbus.rr.com>
2015-05-09 11:31:11 -0400

    improved first two, added third

--- plot1.R
+++ plot1.R
@@ -11,8 +11,18 @@ elec <- read.csv2("household_power_consumption.txt",na.strings=c("?"),dec=".",co
 feb <- elec[elec$date == "1/2/2007" | elec$date == "2/2/2007",]
 
 ## plot 1
-hist(feb[!is.na(feb$active),3],col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
+plotOnCurrentDevice <- function(frame) {
+    hist(frame[!is.na(frame$active),3],
+         col="red",
+         xlab="Global Active Power (kilowatts)",
+         main="Global Active Power"
+    )
+}
 
-## copy to png, in the repository for this assignment
-dev.copy(png,file="../repos/ExData_Plotting1/plot1.png")
+## show it on screen
+plotOnCurrentDevice(feb)
+
+## now create the png, in the repository for this assignment
+png(file="../repos/ExData_Plotting1/plot1.png")
+plotOnCurrentDevice(feb)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -16,23 +16,46 @@ feb <- transform(feb,dt=strptime(paste(date,time),"%d/%m/%Y %H:%M:%S"))
 plotOnCurrentDevice <- function(frame) {
     ## create the plot, suppressing the x-axis,
     ## which will be created in the next statement
-    with(frame,plot(1:nrow(frame),active,type="l",
-            xlab="",ylab="Global Active Power (kilowatts)",
+    with(frame,
+         plot(1:nrow(feb),meter1,type="l",
+            xlab="",ylab="Energy sub metering",
             xaxt="n"
-            ))
+        )
+    )
+    
+    ## red line for the second meter
+    with(frame,
+        lines(1:nrow(feb),meter2,col="red")
+    )
+    
+    ## blue line for the third meter
+    with(frame,
+         lines(1:nrow(feb),meter3,col="blue")
+    )
+
+    ## now the legend - boosting the line width seemed to make
+    ## make the legend a little easier to read
+    legend("topright",
+           c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+           lty="solid",
+           lwd=2,
+           col=c("black","red","blue")
+    )
     
     ## create an x-axis with three tick marks (at midnight)
     ## the labels are computed from the timestamps
     ## that way this script could work with other date ranges
-    ## besides Feb 1-2, 2007
+    ## besides Feb 1-2, 2007, provided there are a full 1,440 
+    ## points per day and we always start at midnight
     ticks <- which(sapply(1:(nrow(frame)+1),function(x)(x %% 1440)==1))
     axis(1,at=ticks,labels=strftime(frame$dt[1]+(ticks-1)*60+1,"%a"))
+
 }
 
-## show it on screen
+## show the plot on screen
 plotOnCurrentDevice(feb)
 
 ## now create the png, in the repository for this assignment
-png(file="../repos/ExData_Plotting1/plot2.png")
+png(file="../repos/ExData_Plotting1/plot3.png")
 plotOnCurrentDevice(feb)
 dev.off()
